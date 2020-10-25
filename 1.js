a043b10702e1e86ee63aa73f43af2b700d6c53dd
TuanFitax <41339074+TuanHcm@users.noreply.github.com>
2020-10-25 21:44:12 +0700

    them2

--- 1.js
+++ 1.js
@@ -1,1 +1,10 @@
-console.log("xin chao");
+var dongho = {
+    hangsx: 'casio',
+    mau: 'den',
+    gia: 5000,
+    gioitinh:'nam',
+    xemngay: function(){
+        return this.hangsx + ' '+ this.gioitinh;
+    }
+};
+console.log(dongho.xemngay());
