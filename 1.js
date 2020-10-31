abfb9e1ad26ed6d5d2e1c38012d95c84f5a73fe6
TuanFitax <41339074+TuanHcm@users.noreply.github.com>
2020-10-31 15:56:26 +0700

    bai 17

--- 1.js
+++ 1.js
@@ -1,11 +1,1 @@
-var hocvien = {
-    ten: "nguyen a",
-    tuoi: 31,
-    chieucao: 170
-};
-console.log(hocvien['ten']);
-var x;
-for (x in hocvien)
-{
-    console.log(hocvien[x]);
-}
+var x = "samsung, iphone, nokia";
